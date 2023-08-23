3d9d3ba58b685e2fe45f8fbdf1d546578262d74c
saerx <saerlaith.robyn@gmail.com>
2023-08-23 17:16:27 +0100

    adds basic audio elements

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/styles/pages.css");
     eleventyConfig.addPassthroughCopy("./src/styles/normalize/normalize.css");
     eleventyConfig.addPassthroughCopy("./src/styles/normalize/the-new-css-reset.css");
+    eleventyConfig.addPassthroughCopy("./src/assets/audio/skateboard.mp3");
     
     return {
         dir: {
