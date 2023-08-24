f767d66c091f4832f2becd0f783b3c71b7f314e2
saerx <saerlaith.robyn@gmail.com>
2023-08-24 11:40:22 +0100

    added disability pride favicon

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,16 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/styles/normalize/normalize.css");
     eleventyConfig.addPassthroughCopy("./src/styles/normalize/the-new-css-reset.css");
     eleventyConfig.addPassthroughCopy("./src/assets/audio/skateboard.mp3");
+    eleventyConfig.addPassthroughCopy("./src/android-chrome-192x192.png");
+    eleventyConfig.addPassthroughCopy("./src/android-chrome-256x256.png");
+    eleventyConfig.addPassthroughCopy("./src/apple-touch-icon.png");
+    eleventyConfig.addPassthroughCopy("./src/browserconfig.xml");
+    eleventyConfig.addPassthroughCopy("./src/favicon-16x16.png");
+    eleventyConfig.addPassthroughCopy("./src/favicon-32x32.png");
+    eleventyConfig.addPassthroughCopy("./src/favicon.ico");
+    eleventyConfig.addPassthroughCopy("./src/mstile-150x150.png");
+    eleventyConfig.addPassthroughCopy("./src/safari-pinned-tab.svg");
+    eleventyConfig.addPassthroughCopy("./src/site.webmanifest");
     
     return {
         dir: {
