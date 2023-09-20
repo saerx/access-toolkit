40da959e2bc3ffd528917c6efeb64073f81ff96b
saerx <saerlaith.robyn@gmail.com>
2023-09-20 14:13:26 +0100

    added audio files for pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,36 +1,3 @@
-const markdownIt = require('markdown-it');
-const markdownItAnchor = require('markdown-it-anchor');
-const markdownTOC = require('markdown-it-toc-done-right');
-
-
-module.exports = function (eleventyConfig) {
-
-    eleventyConfig.amendLibrary("md", mdLib => mdLib.use(markdownItAnchor, { permalink: true, permalinkBefore: true, permalinkSymbol: '§' }).use(markdownTOC));
-    eleventyConfig.addPassthroughCopy("./src/styles/style.css");
-    eleventyConfig.addPassthroughCopy("./src/styles/variables.css");
-    eleventyConfig.addPassthroughCopy("./src/styles/pages.css");
-    eleventyConfig.addPassthroughCopy("./src/styles/normalize/normalize.css");
-    eleventyConfig.addPassthroughCopy("./src/styles/normalize/the-new-css-reset.css");
-    eleventyConfig.addPassthroughCopy("./src/assets/audio/skateboard.mp3");
-    eleventyConfig.addPassthroughCopy("./src/android-chrome-192x192.png");
-    eleventyConfig.addPassthroughCopy("./src/android-chrome-256x256.png");
-    eleventyConfig.addPassthroughCopy("./src/apple-touch-icon.png");
-    eleventyConfig.addPassthroughCopy("./src/browserconfig.xml");
-    eleventyConfig.addPassthroughCopy("./src/favicon-16x16.png");
-    eleventyConfig.addPassthroughCopy("./src/favicon-32x32.png");
-    eleventyConfig.addPassthroughCopy("./src/favicon.ico");
-    eleventyConfig.addPassthroughCopy("./src/mstile-150x150.png");
-    eleventyConfig.addPassthroughCopy("./src/safari-pinned-tab.svg");
-    eleventyConfig.addPassthroughCopy("./src/site.webmanifest");
-    eleventyConfig.addPassthroughCopy("./src/build");
-    eleventyConfig.addPassthroughCopy("./src/button-icons");
-    eleventyConfig.addPassthroughCopy("./src/translations");
-
-    
-    return {
-        dir: {
-            input: "src",
-            output: "public",
-        },
-    };
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:9536b3d04f8bcf79b4df5ba31110b1c55b3cb3b276165a0aa45d244150d25dc4
+size 1758
--- src/pages/other-resources.md
+++ src/pages/other-resources.md
@@ -1,13 +1,3 @@
----
-title: Other resources
-blurb: Discover a range of access resources by disabled artists and artworkers that can further support you on reducing access barriers in the arts, including on how to make an access rider, access suggestions for events and alt text. 
-date: 2023-06-06
----
-- [Access Docs for Artists](https://www.accessdocsforartists.com/) by Leah Clements, Alice Hattrick and Lizzy Rose.
-- [Accessibility in the Arts: A Promise and a Practice](https://promiseandpractice.art/) by Carolyn Lazard.
-- [Access Suggestions for Events](https://www.sinsinvalid.org/blog/access-suggestions-for-a-public-event) by Sins Invalid.
-- [Alt Text as Poetry](https://alt-text-as-poetry.net/#book) by Bojana Coklyat and Shannon Finnegan.
-- [Creating Your Own Access Rider](https://weareunlimited.org.uk/resource/creating-your-own-access-rider/) by Unlimited.  
-- [This Work Isn’t For Us](https://docs.google.com/document/d/1HGBSsBsERxSaD1t0Oq_9acqAqiAPPLekBxaJ8tk-Njw/edit) by Jemma Desai.   
-- [Access Webinars](https://www.youtube.com/@ADIarts) by Arts and Disability Ireland.
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:ecb6d055df724c1795b95fe6befe804ec5e80de545b784c65fd715c9bf80e40b
+size 1095
