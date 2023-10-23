272fad34d52ffdc48b56d7245a153f3bf7f6ac6c
saerx <saerlaith.robyn@gmail.com>
2023-10-23 11:50:28 +0100

    fixed git lfs tracking too many files

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,37 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5ba8eef894568ba9ebbfb0af24c4ba597dc50eeabcfa7f9bb2b8b6df5891ae5a
-size 1817
+const markdownIt = require('markdown-it');
+const markdownItAnchor = require('markdown-it-anchor');
+const markdownTOC = require('markdown-it-toc-done-right');
+
+
+module.exports = function (eleventyConfig) {
+
+    eleventyConfig.amendLibrary("md", mdLib => mdLib.use(markdownItAnchor, { permalink: true, permalinkBefore: true, permalinkSymbol: '§' }).use(markdownTOC));
+    eleventyConfig.addPassthroughCopy("./src/styles/style.css");
+    eleventyConfig.addPassthroughCopy("./src/styles/variables.css");
+    eleventyConfig.addPassthroughCopy("./src/styles/pages.css");
+    eleventyConfig.addPassthroughCopy("./src/styles/normalize/normalize.css");
+    eleventyConfig.addPassthroughCopy("./src/styles/normalize/the-new-css-reset.css");
+    eleventyConfig.addPassthroughCopy("./src/assets/audio");
+    eleventyConfig.addPassthroughCopy("./src/assets/img");
+    eleventyConfig.addPassthroughCopy("./src/android-chrome-192x192.png");
+    eleventyConfig.addPassthroughCopy("./src/android-chrome-256x256.png");
+    eleventyConfig.addPassthroughCopy("./src/apple-touch-icon.png");
+    eleventyConfig.addPassthroughCopy("./src/browserconfig.xml");
+    eleventyConfig.addPassthroughCopy("./src/favicon-16x16.png");
+    eleventyConfig.addPassthroughCopy("./src/favicon-32x32.png");
+    eleventyConfig.addPassthroughCopy("./src/favicon.ico");
+    eleventyConfig.addPassthroughCopy("./src/mstile-150x150.png");
+    eleventyConfig.addPassthroughCopy("./src/safari-pinned-tab.svg");
+    eleventyConfig.addPassthroughCopy("./src/site.webmanifest");
+    eleventyConfig.addPassthroughCopy("./src/build");
+    eleventyConfig.addPassthroughCopy("./src/button-icons");
+    eleventyConfig.addPassthroughCopy("./src/translations");
+
+    
+    return {
+        dir: {
+            input: "src",
+            output: "public",
+        },
+    };
+};
--- src/index.md
+++ src/index.md
@@ -1,3 +1,10 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:df1b41c4642bacb7a75617dbde7c2bfa7be9ca8b8b0b3fed1ed446b12c0a10ec
-size 162
+---
+title: Access Toolkit for Arts Workers
+layout: "home.njk"
+---
+
+{% for page in collections.pages %}
+
+- ### [{{ page.data.title}}]({{ page.url }})
+
+{% endfor %}
